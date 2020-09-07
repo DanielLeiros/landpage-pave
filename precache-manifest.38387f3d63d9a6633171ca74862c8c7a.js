e1e42c1056fde46b0f71d0eeda35cd0d211be297
Daniel Leiros <danielmleiros@hotmail.com>
2020-09-07 03:24:18 -0300

    Updates

---
+++ precache-manifest.38387f3d63d9a6633171ca74862c8c7a.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d86808b4f27de932f866815d1a2ea2c",
+    "url": "/landpage-pave/index.html"
+  },
+  {
+    "revision": "1f86ece090e60672f709",
+    "url": "/landpage-pave/static/css/main.009a6707.chunk.css"
+  },
+  {
+    "revision": "b9ced6e9e3efa5663735",
+    "url": "/landpage-pave/static/js/2.16b6b3f5.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/landpage-pave/static/js/2.16b6b3f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f86ece090e60672f709",
+    "url": "/landpage-pave/static/js/main.d487fc56.chunk.js"
+  },
+  {
+    "revision": "a06abbd12fa33f0ad8cf",
+    "url": "/landpage-pave/static/js/runtime-main.e37f68bc.js"
+  },
+  {
+    "revision": "e02846402d7b3ef5842fe09195dd4a48",
+    "url": "/landpage-pave/static/media/background-shapes.e0284640.png"
+  },
+  {
+    "revision": "d05a743924176715e537e22e4f6e4406",
+    "url": "/landpage-pave/static/media/bubbles.d05a7439.svg"
+  },
+  {
+    "revision": "9e505fa1957d31eaaff1594c92852734",
+    "url": "/landpage-pave/static/media/clientes.9e505fa1.svg"
+  },
+  {
+    "revision": "3761584dbae448e8ea9f8d05e8fea756",
+    "url": "/landpage-pave/static/media/coin-dollar.3761584d.svg"
+  },
+  {
+    "revision": "e6da682c44f169692ba14a3edc14d640",
+    "url": "/landpage-pave/static/media/fotos-principal.e6da682c.png"
+  },
+  {
+    "revision": "f2cf393bc298ff52735249f5bab752bd",
+    "url": "/landpage-pave/static/media/leaf.f2cf393b.svg"
+  },
+  {
+    "revision": "359cd9cf4095b1468b608ccdee5b3bd7",
+    "url": "/landpage-pave/static/media/list.359cd9cf.svg"
+  },
+  {
+    "revision": "cb907c36a103371e644995b33f661696",
+    "url": "/landpage-pave/static/media/logo.cb907c36.png"
+  },
+  {
+    "revision": "3024d13c622dceaf97f006fdd6bbd866",
+    "url": "/landpage-pave/static/media/restaurantes.3024d13c.svg"
+  },
+  {
+    "revision": "8bb67e30271e77e88b0ba26fb73bdbfa",
+    "url": "/landpage-pave/static/media/rocket.8bb67e30.svg"
+  },
+  {
+    "revision": "7207a24cef135b5233b96d0f3bc8d0d4",
+    "url": "/landpage-pave/static/media/stopwatch.7207a24c.svg"
+  },
+  {
+    "revision": "ae228834a1d6f6d0757d42e185fd8472",
+    "url": "/landpage-pave/static/media/ticket.ae228834.svg"
+  },
+  {
+    "revision": "6c9f5eafeed4d7057a7f373156fda8cc",
+    "url": "/landpage-pave/static/media/tree.6c9f5eaf.svg"
+  }
+]);
