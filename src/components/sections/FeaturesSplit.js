09327452fd8a6480c5cd4aa07d03931f687f82e6
Daniel Leiros <danielmleiros@hotmail.com>
2020-10-04 03:02:36 -0300

    Add new section and form fix ~but not working yet

---
+++ src/components/sections/FeaturesSplit.js
@@ -0,0 +1,128 @@
+import React from 'react';
+import classNames from 'classnames';
+import { SectionSplitProps } from '../../utils/SectionProps';
+import SectionHeader from './partials/SectionHeader';
+import Image from '../elements/Image';
+
+const propTypes = {
+  ...SectionSplitProps.types
+}
+
+const defaultProps = {
+  ...SectionSplitProps.defaults
+}
+
+const FeaturesSplit = ({
+  className,
+  topOuterDivider,
+  bottomOuterDivider,
+  topDivider,
+  bottomDivider,
+  hasBgColor,
+  invertColor,
+  invertMobile,
+  invertDesktop,
+  alignTop,
+  imageFill,
+  ...props
+}) => {
+
+  const outerClasses = classNames(
+    'features-split section',
+    topOuterDivider && 'has-top-divider',
+    bottomOuterDivider && 'has-bottom-divider',
+    hasBgColor && 'has-bg-color',
+    invertColor && 'invert-color',
+    className
+  );
+
+  const innerClasses = classNames(
+    'features-split-inner section-inner',
+    topDivider && 'has-top-divider',
+    bottomDivider && 'has-bottom-divider'
+  );
+
+  const splitClasses = classNames(
+    'split-wrap',
+    invertMobile && 'invert-mobile',
+    invertDesktop && 'invert-desktop',
+    alignTop && 'align-top'
+  );
+
+  const sectionHeader = {
+    title: 'O que é o pavê',
+    paragraph: 'O pavê é um aplicativo que através das suas telas, lhe dará acesso facilmente ao cardápio do estabelecimento que estiver querendo ver naquele momento. Até de um carrinho de cachorro quente perto de você, se quiser'
+  };
+
+  return (
+    <section
+      {...props}
+      className={outerClasses}
+    >
+      <div className="container">
+        <div className={innerClasses}>
+          <SectionHeader data={sectionHeader} className="center-content" />
+          <div className={splitClasses}>
+
+            <div className="split-item">
+              <div className="split-item-content center-content-mobile reveal-from-left" data-reveal-container=".split-item">
+              <div className="text-xxs text-color-secondary fw-600 tt-u mb-8">
+                  Pra quem gosta de viver boas experiências gastronômicas
+                  </div>
+                <h3 className="mt-0 mb-12">
+                  Salve tempo e dinheiro
+                  </h3>
+                <p className="m-0">
+                Cansado de ficar caçando os preços e pratos dos restaurantes em sites ou redes sociais de cada um? 
+                  </p>
+              </div>
+              <div className={
+                classNames(
+                  'split-item-image center-content-mobile reveal-from-bottom',
+                  imageFill && 'split-item-image-fill'
+                )}
+                data-reveal-container=".split-item">
+                <Image
+                  src={require('./../../assets/images/pic-exemple-1.png')}
+                  alt="Features split 01"
+                  width={528}
+                  height={396} />
+              </div>
+            </div>
+
+            <div className="split-item">
+              <div className="split-item-content center-content-mobile reveal-from-right" data-reveal-container=".split-item">
+                <div className="text-xxs text-color-secondary fw-600 tt-u mb-8">
+                  Pra quem quer alavancar seu negócio e facilitar a vida para seus clientes
+                  </div>
+                <h3 className="mt-0 mb-12">
+                  Ganhe visibilidade e praticidade para seu negócio
+                  </h3>
+                <p className="m-0">
+                Torne seu cardápio digital, sendo mais atrativo e acessível pelos seus clientes. Faça com que suas promoções se espalhem. Ganhe espaço e visibilidade através dos nossos rankings. 
+                  </p>
+              </div>
+              <div className={
+                classNames(
+                  'split-item-image center-content-mobile reveal-from-bottom',
+                  imageFill && 'split-item-image-fill'
+                )}
+                data-reveal-container=".split-item">
+                <Image
+                  src={require('./../../assets/images/pic-exemple-2.png')}
+                  alt="Features split 02"
+                  width={528}
+                  height={396} />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+FeaturesSplit.propTypes = propTypes;
+FeaturesSplit.defaultProps = defaultProps;
+
+export default FeaturesSplit;
